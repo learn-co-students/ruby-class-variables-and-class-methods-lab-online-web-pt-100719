123c18c93a7b93c9080f352f4edb7a5e451eec9a
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-18 15:42:06 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song 
+  @@count = 0
+  
+  def self.count 
+    @@count
+  end 
+  
+   @@genres = []
+   
+   def self.genres
+    @@genres.uniq
+  end
+   
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+  @@artists = []
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  attr_accessor :name, :artist, :genre
+end 
+
+hero = Song.new("Escape", "Enrique Iglesias", "romance")
+
+puts hero.name
+puts hero.artist
+puts hero.genre
+puts Song.count
+Song.genres
+Song.artists
+Song.genre_count
+Song.artist_count
