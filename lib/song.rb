112fe43706f409ee70bd27a39952cdb7107a9336
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-30 00:44:56 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+
+def initialize(name, artist, genre)
+@name = name
+@artist = artist
+@genre = genre
+@@count += 1
+end
+
+attr_accessor :name, :artist, :genre
+
+@@count = 0
+
+def 
+
+end
