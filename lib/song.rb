0ec5941cf4c7b4c157b0439d2f22dabf7739c2b5
Derric Fragosa <fderric@gmail.com>
2019-11-03 07:47:11 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  
+  @@count = 0
+  
+  def self.count
+    @@count 
+  end 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre 
+  @@count +=1 
+  end 
+  
+  
+ 
+  
+  
+end 
+
+song = Song.new("song_title","artist", "genre")
