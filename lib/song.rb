2689a6c88bfdd213a3cf9773302eaab260c3e5a7
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-11-08 04:59:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,19 +6,13 @@ class Song
   @@artists = []
   @@genres = []
 
-  # Create our initialize method
-  # Keeps track of a song's name, artist and genre
-  def initialize(name, artist, genre)
-    @name =  name
+  def initialize(song_name, artist, genre)
+    @name = song_name
     @artist = artist
     @genre = genre
-    if !(@@artists.include?(@artist))
-      @@artists << @artist
-    end
-    if !(@@genres.include?(@genre))
-        @@genres << @genre
-    end
     @@count += 1
+    @@genres << genre
+    @@artists << artist
   end
 
   def self.count
@@ -26,15 +20,35 @@ class Song
   end
 
   def self.genres
-    @@genres
-  end
-
-  def self.genre_count
-    @@genres
+    @@genres.uniq!
   end
 
   def self.artists
-    @@artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
 end
